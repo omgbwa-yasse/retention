b909015bccd77ca684c1d421cefc96056b0ddfe3
omgbwa-yasse <omgbwayasse@gmail.com>
2024-04-08 21:41:54 +0100

    ajout protect dans model Reference

--- app/Models/Reference.php
+++ app/Models/Reference.php
@@ -16,6 +16,8 @@ class Reference extends Model
         'category_id',
     ];
 
+    protected $table = 'reference';
+
     public function category()
     {
         return $this->belongsTo(ReferenceCategory::class);
