66035721104d3910283e11048002c4fcff678535
Diandreas <davidnjandjeu@gmail.com>
2024-04-21 22:46:04 +0100

    connexion

--- resources/views/menuTop.blade.php
+++ resources/views/menuTop.blade.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0c9172;">
     <div class="container-fluid">
         <a class="navbar-brand" href="#">African Retention Portail</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
@@ -17,9 +18,46 @@
                 </li>
             </ul>
             <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="règle, Classe, Durée" aria-label="Search">
+                <input class="form-control mr-sm-2" type="search" placeholder="règle, Classe, Durée"
+                    aria-label="Search">
                 <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Recherche</button>
             </form>
         </div>
     </div>
+    <!-- Right Side Of Navbar -->
+    <ul class="navbar-nav ms-auto">
+        <!-- Authentication Links -->
+        @guest
+            @if (Route::has('login'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+            @endif
+
+            @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
+            @endif
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+    </ul>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,120 +1,78 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\MissionController;
+use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\ReferenceController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ReferenceCategoryController;
+use App\Http\Controllers\RessourceController;
+use App\Http\Controllers\TypologyController;
+use App\Http\Controllers\SettingController;
+use App\Http\Controllers\RuleController;
+use App\Http\Controllers\ActiveController;
+use App\Http\Controllers\DuaController;
+use App\Http\Controllers\DulController;
+use App\Http\Controllers\DulArticleController;
+use App\Http\Controllers\RuleClassificationController;
+use App\Http\Controllers\ForumController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ValidationController;
 
-Route::get('/', function () {
-    return view('index');
+Auth::routes();
+
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', function () {
+        return view('index');
+    });
+
+    Route::get('/search', function () {
+        return view('search.searchActivity');
+    })->name('searchActivity');
+
+    Route::get('/add', function () {
+        return view('search.searchActivity');
+    })->name('addActivity');
+
+    Route::get('/setting', function () {
+        return view('search.searchActivity');
+    })->name('setting');
+
+    Route::get('/forum', function () {
+        return view('search.searchActivity');
+    })->name('forum');
+
+    Route::resource('mission', MissionController::class);
+    Route::resource('activity', ActivityController::class);
+    Route::resource('reference', ReferenceController::class);
+
+    Route::get('reference/{reference}/article/create', [ArticleController::class, 'create'])->name('article.create');
+    Route::post('reference/{reference}/article', [ArticleController::class, 'store'])->name('article.store');
+    Route::get('reference/{reference}/article/{article}', [ArticleController::class, 'show'])->name('article.show');
+    Route::get('reference/{reference}/article/{article}/edit', [ArticleController::class, 'edit'])->name('article.edit');
+    Route::put('reference/{reference}/article/{article}', [ArticleController::class, 'update'])->name('article.update');
+    Route::delete('reference/{reference}/article/{article}', [ArticleController::class, 'destroy'])->name('article.destroy');
+    Route::get('references/{reference}/article', [ArticleController::class, 'index'])->name('article.index');
+
+    Route::resource('reference-category', ReferenceCategoryController::class);
+    Route::resource('ressource', RessourceController::class);
+    Route::resource('typology', TypologyController::class);
+    Route::resource('setting', SettingController::class);
+    Route::resource('rule', RuleController::class);
+    Route::resource('active', ActiveController::class);
+    Route::resource('rule.dua', DuaController::class);
+    Route::resource('rule.dul', DulController::class);
+    Route::resource('rule.dul.dulreference', DulArticleController::class)->only(['create', 'store', 'show', 'edit', 'update', 'destroy']);
+    Route::resource('rule.classification', RuleClassificationController::class)->only(['create', 'store', 'show', 'edit', 'update', 'destroy']);
+    Route::resource('forum', ForumController::class);
+    Route::resource('user', UserController::class);
+    Route::resource('validation', ValidationController::class);
 });
 
-
-
-// Routes sans préfixe
-Route::get('/search', function () {
-    return view('search.searchActivity');
-})->name('searchActivity');
-
-Route::get('/add', function () {
-    return view('search.searchActivity');
-})->name('addActivity');
-
-Route::get('/setting', function () {
-    return view('search.searchActivity');
-})->name('setting');
-
-Route::get('/forum', function () {
-    return view('search.searchActivity');
-})->name('forum');
-
-
-
-use App\Http\Controllers\MissionController;
-Route::resource('mission', MissionController::class);
-
-
-use App\Http\Controllers\ActivityController;
-Route::resource('activity', ActivityController::class);
-
-use App\Http\Controllers\ReferenceController;
-Route::resource('reference', ReferenceController::class);
-
-
-use App\Http\Controllers\ArticleController;
-
-Route::get('reference/{reference}/article/create', [ArticleController::class, 'create'])->name('article.create');
-Route::post('reference/{reference}/article', [ArticleController::class, 'store'])->name('article.store');
-Route::get('reference/{reference}/article/{article}', [ArticleController::class, 'show'])->name('article.show');
-Route::get('reference/{reference}/article/{article}/edit', [ArticleController::class, 'edit'])->name('article.edit');
-Route::put('reference/{reference}/article/{article}', [ArticleController::class, 'update'])->name('article.update');
-Route::delete('reference/{reference}/article/{article}', [ArticleController::class, 'destroy'])->name('article.destroy');
-Route::get('references/{reference}/article', [ArticleController::class, 'index'])->name('article.index');
-
-
-use App\Http\Controllers\ReferenceCategoryController;
-Route::resource('reference-category', ReferenceCategoryController::class);
-
-use App\Http\Controllers\RessourceController;
-Route::resource('ressource', RessourceController::class);
-
-use App\Http\Controllers\TypologyController;
-Route::resource('typology', TypologyController::class);
-
-
-use App\Http\Controllers\SettingController;
-Route::resource('setting', SettingController::class);
-
-
-use App\Http\Controllers\RuleController;
-Route::resource('rule', RuleController::class);
-
-use App\Http\Controllers\activeController;
-Route::resource('active', ActiveController::class);
-
-use App\Http\Controllers\DuaController;
-Route::resource('rule.dua', DuaController::class);
-
-
-use App\Http\Controllers\DulController;
-Route::resource('rule.dul', DulController::class);
-
-
-use App\Http\Controllers\DulArticleController;
-Route::resource('rule.dul.dulreference', DulArticleController::class)->only(['create', 'store', 'show', 'edit', 'update', 'destroy']);
-
-use App\Http\Controllers\RuleClassificationController;
-Route::resource('rule.classification', RuleClassificationController::class)->only(['create', 'store', 'show', 'edit', 'update', 'destroy']);
-
-
-use App\Http\Controllers\ForumController;
-Route::resource('forum', ForumController::class);
-
-use App\Http\Controllers\UserController;
-Route::resource('user', UserController::class);
-
-
-use App\Http\Controllers\ValidationController;
-Route::resource('validation', ValidationController::class);
-
-
-
-
-
-// Dans le fichier routes/web.php,,,,,,,,,,,,,,,,,,,'
-/*Route::get('/reference/{id}', 'ReferenceController@show')->name('reference.show');*/
-
-
-
-
-    Auth::routes();
-
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-    Auth::routes();
-
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-    Auth::routes();
-
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-
-
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
