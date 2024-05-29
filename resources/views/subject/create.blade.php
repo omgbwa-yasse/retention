ba52051d7c237415a67fd8ccdf67cf5bc3cf4c4b
Diandreas <davidnjandjeu@gmail.com>
2024-05-29 07:53:00 +0100

    Revert "ajout"
    This reverts commit 244177922e2d94b62acf9e3eacb6ecd6ec3f3a84.

--- resources/views/subject/create.blade.php
+++ resources/views/subject/create.blade.php
@@ -3,15 +3,6 @@
 <h1>Ajouter un sujet</h1>
         <form action="{{ route('subject.store') }}" method="POST">
             @csrf
-            <div class="mb-3">
-                <label for="" class="form-label">Activités concernées</label>
-                <select class="form-select form-select-lg" name="class_id" id="" >
-                    @foreach($classes as $classe)
-                        <option value="{{ $classe->id }}">{{ $classe->code }} - {{ $classe->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-
             <div class="form-group">
                 <label for="name">Name:</label>
                 <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
@@ -21,6 +12,16 @@
                 <label for="description">Description:</label>
                 <textarea name="description" id="description" class="form-control">{{ old('description') }}</textarea>
             </div>
+
+            <div class="mb-3">
+                <label for="" class="form-label">Classes</label>
+                <select class="form-select form-select-lg" name="class_id" id="" >
+                    @foreach($classes as $classe)
+                        <option value="{{ $classe->id }}">{{ $classe->code }} - {{ $classe->name }}</option>
+                    @endforeach
+                </select>
+            </div>
+
 
             <button type="submit" class="btn btn-primary">Create</button>
             <button type="reset" class="btn btn-primary">Annuler</button>
