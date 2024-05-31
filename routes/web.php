e8b197678a05cb548ae2491d9ca135255b223d42
Diandreas <davidnjandjeu@gmail.com>
2024-05-31 09:40:19 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -73,7 +73,7 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/reaction/{post}', [ForumReactionController::class, 'add'])->name('reaction.add');
     Route::post('/subject/{subject}/post/{post}/reply', [ForumPostController::class, 'reply'])->name('subject.post.reply');
     Route::get('/subject/{subject}/post/{post}', [ForumPostController::class, 'show'])->name('subject.post.show');
-   
+
     Route::get('/subject/{subject}/post/{post}/edit', [ForumPostController::class, 'edit'])->name('subject.post.edit');
 
 
