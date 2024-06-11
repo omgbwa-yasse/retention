461a455a2e0378a2ab4e077e2da7ae1cc0818c87
Diandreas <127276266+Diandreas@users.noreply.github.com>
2024-06-11 15:47:10 +0100

    Update index.blade.php

--- resources/views/country/index.blade.php
+++ resources/views/country/index.blade.php
@@ -21,15 +21,25 @@
                         @endif
 
                         @foreach ($Countries as $country)
-                            <div class="media">
-                                <div class="media-body">
-                                    <h5 class="mt-0"><a
-                                            href="{{ route('country.show', $country->id) }}">{{ $country->name }}</a>
-                                    </h5>
-                                    <p class="text-muted">Abbreviation: {{ $country->abbr }}</p>
+                                <div class="media">
+                                    <div class="media-body">
+                                        <h5 class="mt-0"><a href="{{ route('country.show', $country->id) }}">{{ $country->name }}</a></h5>
+                                        <p class="text-muted">Abbreviation: {{ $country->abbr }}</p>
+
+                                        @if(auth()->check())
+                                            <a href="{{ route('country.edit', $country->id) }}" class="btn btn-secondary btn-sm">{{ __('Edit') }}</a>
+
+                                            <form action="{{ route('country.destroy', $country->id) }}" method="POST" class="d-inline-block">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm('{{ __("Are you sure you want to delete this country?") }}')">{{ __('Delete') }}</button>
+                                            </form>
+                                        @endif
+                                    </div>
                                 </div>
-                            </div>
-                            <hr>
+                                <hr>
+
+                                <hr>
                         @endforeach
                     </div>
                 </div>
