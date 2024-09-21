be46de4feeecb10ddda51a4b8432e582b803ee80
omgbwa-yasse <omgbwayasse@gmail.com>
2024-09-21 08:03:30 +0100

    Nettoyage phase 2

--- app/Http/Controllers/RuleController.php
+++ app/Http/Controllers/RuleController.php
@@ -8,14 +8,9 @@ use App\Models\Rule;
 use App\Models\Country;
 use App\Models\Articles;
 use App\Models\status;
-use App\Models\Active;
-use App\Models\Dua;
 use App\Models\Dul;
 use App\Models\Trigger;
 use App\Models\Sort;
-use App\Models\RuleActive;
-use App\Models\RuleDua;
-use App\Models\RuleDul;
 use Illuminate\Support\Facades\Auth;
 
 class RuleController extends Controller
@@ -45,7 +40,6 @@ class RuleController extends Controller
         $states = status::all();
         $triggers = trigger::all();
         $sorts = Sort::all();
-        $actives = Active::all();
         $articles = Articles ::all()->where('country_id','=',Auth::user()->country_id);
         $countries = country::orderBy('name')->get();
         return view('rule.ruleCreate', compact('countries','triggers','actives','articles','countries','sorts','states'));
@@ -60,12 +54,6 @@ class RuleController extends Controller
             'code' => 'required',
             'name' => 'required',
             'description' => 'nullable',
-            'active_duration' => 'required',
-            'active_trigger' => 'required',
-            'active_sort' => 'required',
-            'dua_duration' => 'required',
-            'dua_trigger' => 'required',
-            'dua_sort' => 'required',
             'dul_duration' => 'required',
             'dul_trigger' => 'required',
             'dul_sort' => 'required'
@@ -86,25 +74,6 @@ class RuleController extends Controller
 
 
         $ruleId = $rule->id;
-        Active::create([
-            'duration' => $request->input('active_duration'),
-            'description' => $request->input('active_description'),
-            'trigger_id' => $request->input('active_trigger'),
-            'sort_id' => $request->input('active_sort'),
-            'rule_id' => $ruleId,
-            'country_id' => $countryId,
-            'user_id' => Auth::user()->id
-        ]);
-
-        Dua::create([
-            'duration' => $request->input('dua_duration'),
-            'description' => $request->input('dua_description'),
-            'trigger_id' => $request->input('dua_trigger'),
-            'sort_id' => $request->input('dua_sort'),
-            'rule_id' => $ruleId,
-            'country_id' => $countryId,
-            'user_id' => Auth::user()->id
-        ]);
 
         Dul::create([
             'duration' => $request->input('dul_duration'),
@@ -158,12 +127,6 @@ class RuleController extends Controller
             'code' => 'required',
             'name' => 'required',
             'description' => 'nullable',
-            'active_duration' => 'required',
-            'active_trigger' => 'required',
-            'active_sort' => 'required',
-            'dua_duration' => 'required',
-            'dua_trigger' => 'required',
-            'dua_sort' => 'required',
             'dul_duration' => 'required',
             'dul_trigger' => 'required',
             'dul_sort' => 'required',
@@ -188,26 +151,6 @@ class RuleController extends Controller
             'status_id' => 1
         ]);
 
-        Active::where('rule_id', $id)
-              ->update([
-                  'duration' => $request->input('active_duration'),
-                  'description' => $request->input('active_description'),
-                  'trigger_id' => $request->input('active_trigger'),
-                  'sort_id' => $request->input('active_sort'),
-                  'country_id' => $countryId,
-                  'user_id' => Auth::user()->id
-              ]);
-
-        Dua::where('rule_id', $id)
-           ->update([
-               'duration' => $request->input('dua_duration'),
-               'description' => $request->input('dua_description'),
-               'trigger_id' => $request->input('dua_trigger'),
-               'sort_id' => $request->input('dua_sort'),
-               'country_id' => $countryId,
-               'user_id' => Auth::user()->id
-           ]);
-
         Dul::where('rule_id', $id)
            ->update([
                'duration' => $request->input('dul_duration'),
@@ -227,15 +170,6 @@ class RuleController extends Controller
     // Supprime un élément spécifique
     public function destroy(Rule $rule)
     {
-        $active = Active::where('rule_id', $rule->id)->get();
-        foreach ($active as $a) {
-            $a->delete();
-        }
-
-        $dua = Dua::where('rule_id', $rule->id)->get();
-        foreach ($dua as $d) {
-            $d->delete();
-        }
 
         $dul = Dul::where('rule_id', $rule->id)->get();
         foreach ($dul as $d) {
@@ -246,6 +180,7 @@ class RuleController extends Controller
 
         return redirect()->route('rule.index')->with('success', 'Rule deleted successfully.');
     }
+
     public function export()
     {
         $rules = Rule::with('status')->get();
@@ -254,7 +189,5 @@ class RuleController extends Controller
 
         return $pdf->download('regles_de_conservation.pdf');
     }
-
-
 
 }
