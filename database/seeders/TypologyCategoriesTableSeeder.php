b7f6accdbab8e50063d16c0817ccb47d3d5faea3
omgbwa-yasse <omgbwayasse@gmail.com>
2025-01-15 16:06:07 -0500

    feat: add seeder for typology categories to populate database

---
+++ database/seeders/TypologyCategoriesTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TypologyCategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run()
+    {
+        $categories = [
+            ['id' => 1, 'name' => 'Administratif', 'description' => 'Catégorie des typologies administratives','user_id' => 1],
+            ['id' => 2, 'name' => 'Technique', 'description' => 'Catégorie des typologies techniques','user_id' => 1],
+            ['id' => 3, 'name' => 'Communication', 'description' => 'Catégorie des typologies de communication','user_id' => 1]
+        ];
+
+        DB::table('typology_categories')->insert($categories);
+    }
+}
