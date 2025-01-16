cf92f28457b7d3d056c21eb23baa631d4d932e05
Diandreas <127276266+Diandreas@users.noreply.github.com>
2025-01-16 11:14:52 +0100

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,2 +1,26 @@
-<footer style="background-color: #f8f9fa; padding: 20px; text-align: center; font-family: Arial, sans-serif;"> <p style="font-size: 14px; color: #333; margin: 0;"> Le projet de Portail africain des délais de conservation des documents administratifs a reçu le soutien de la Commission du Programme du Conseil International des Archives (ICA)</p> </footer>
+<footer style="background-color: #f8f9fa; padding: 20px; text-align: center; font-family: Arial, sans-serif;">
+    <svg width="46" height="15" viewBox="0 0 76 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <mask id="mask0_6_7" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="34" y="4" width="9" height="9">
+            <path d="M35.5148 12.866L34.3418 11.693L41.4974 4.53813L42.6699 5.71114L35.5148 12.866Z" fill="white"/>
+        </mask>
+        <g mask="url(#mask0_6_7)">
+            <path d="M34.3418 12.866H42.6699V4.53813H34.3418V12.866Z" fill="#A7E1D9"/>
+        </g>
+        <path d="M17.9211 30.4602L16.748 29.2872L23.9034 22.1322L25.076 23.3052L17.9211 30.4602Z" fill="#369197"/>
+        <path d="M41.4967 30.4602L34.3418 23.3052L35.5144 22.1322L42.67 29.2872L41.4967 30.4602Z" fill="#369197"/>
+        <path d="M23.903 12.866L16.748 5.71114L17.9206 4.53813L25.076 11.693L23.903 12.866Z" fill="#55B6B3"/>
+        <path d="M12.209 18.3279H22.3283V16.6696H12.209V18.3279Z" fill="#3D9EA1"/>
+        <path d="M28.8809 34.9998H30.539V24.88H28.8809V34.9998Z" fill="#369197"/>
+        <path d="M28.8809 10.1177H30.539V-0.00150299H28.8809V10.1177Z" fill="#79CCC5"/>
+        <path d="M13.9252 25.0999L13.2773 23.5737L22.5918 19.6198L23.2397 21.1461L13.9252 25.0999Z" fill="#369197"/>
+        <path d="M35.7839 33.9324L31.8301 24.6179L33.3563 23.9702L37.3102 33.2846L35.7839 33.9324Z" fill="#369197"/>
+        <path d="M26.0633 11.0281L22.1094 1.71308L23.6351 1.06519L27.5894 10.3802L26.0633 11.0281Z" fill="#67C2BD"/>
+        <path d="M22.5558 15.5027L13.1738 11.7119L13.7946 10.1747L23.1765 13.9655L22.5558 15.5027Z" fill="#48ABAB"/>
+        <path d="M23.922 34.0356L22.3848 33.4149L26.1755 24.0323L27.7127 24.6537L23.922 34.0356Z" fill="#369197"/>
+        <path d="M33.2442 10.9656L31.707 10.3447L35.4978 0.962151L37.0348 1.58363L33.2442 10.9656Z" fill="#8FD7D0"/>
+        <path d="M0 33.7732H5.31958V0.95945H0V33.7732Z" fill="#369197"/>
+        <path d="M63.4152 0.960022H57.0516L44.6719 33.7732H50.1407L53.2732 25.1719H67.0444L70.1769 33.7732H75.7946L63.4152 0.960022ZM60.1837 5.53411L65.553 20.8466H54.7646L60.1837 5.53411Z" fill="#369197"/>
+    </svg>
+
+    <p style="font-size: 14px; color: #333; margin: 0;"> Le projet de Portail africain des délais de conservation des documents administratifs a reçu le soutien de la Commission du Programme du Conseil International des Archives (ICA)</p> </footer>
 
