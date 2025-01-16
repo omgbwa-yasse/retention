91753c213b12c33d97d035accc9e7ec4cc530a96
Diandreas <127276266+Diandreas@users.noreply.github.com>
2025-01-16 11:05:37 +0100

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,1 +1,2 @@
-<footer style="background-color: #f8f9fa; padding: 20px; text-align: center; font-family: Arial, sans-serif;"> <p style="font-size: 14px; color: #333; margin: 0;"> this project support by PCOM &copy; ICA 2024</p> </footer>
+<footer style="background-color: #f8f9fa; padding: 20px; text-align: center; font-family: Arial, sans-serif;"> <p style="font-size: 14px; color: #333; margin: 0;"> Le projet de Portail africain des délais de conservation des documents administratifs a reçu le soutien de la Commission du Programme du Conseil International des Archives (ICA)</p> </footer>
+
