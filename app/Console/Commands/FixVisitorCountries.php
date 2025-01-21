3dd634004779b7dd5bed922302636f33ce9f7fcc
Diandreas <127276266+Diandreas@users.noreply.github.com>
2025-01-21 08:19:09 +0100

    geo

---
+++ app/Console/Commands/FixVisitorCountries.php
@@ -0,0 +1,29 @@
+<?php
+// Dans app/Console/Commands/FixVisitorCountries.php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class FixVisitorCountries extends Command
+{
+    protected $signature = 'visitors:fix-countries';
+    protected $description = 'Fix unknown country entries in visitor stats';
+
+    public function handle()
+    {
+        $this->info('Starting to fix visitor countries...');
+
+        // Mettre à jour les entrées "Unknown"
+        DB::table('visitor_stats')
+            ->where('country_code', 'XX')
+            ->orWhere('country_name', 'Unknown')
+            ->update([
+                'country_code' => 'NI',
+                'country_name' => 'Non Identifie'
+            ]);
+
+        $this->info('Finished fixing visitor countries.');
+    }
+}
